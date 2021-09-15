8aaf032610d6e88cff2d11cb49cc4b59bf7b8d7d
Severino de Souza Souza <severino_souza@outlook.com>
2021-09-14 21:34:37 -0400

    Add classe VirtualstoreDatabaseSettings.cs na pasta models

---
+++ StoreApi/Models/VirtualstoreDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace StoreApi.Models
+{
+    public class VirtualstoreDatabaseSettings : IDatabaseSettings
+    {
+        public string StoreCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IDatabaseSettings
+    {
+        string StoreCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
